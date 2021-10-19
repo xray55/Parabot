c5e236793013fbe54f662dabcb0c9d39742a6e6a
Zachary Martin <Zach.martin.paul@live.com>
2021-10-19 16:06:15 -0500

    Add files via upload

--- AioFisher.java
+++ AioFisher.java
@@ -7,6 +7,7 @@ import org.parabot.environment.scripts.ScriptManifest;
 import org.parabot.environment.scripts.framework.Strategy;
 import org.rev317.min.api.methods.*;
 import org.rev317.min.api.wrappers.Item;
+import org.rev317.min.api.wrappers.Npc;
 
 import java.util.ArrayList;
 @ScriptManifest(author = "Blade", category = Category.FISHING, description = "Fishes stuff", name = "AioFisher", servers = { "2006Scape" }, version = 1)
@@ -14,8 +15,8 @@ import java.util.ArrayList;
 public class AioFisher extends Script {
 
     public ArrayList<Strategy> strategies = new ArrayList<Strategy>();
-    private static final int[] FISH_ID = {317,318};
-    private static final int[] FISHS_ID = {317,316,315};
+    private static final int[] FISH_ID = {336,332};
+    public static final int[] FISHS_ID = {317,316,315};
     @Override
     public boolean onExecute() {
         strategies.add(new Fishing());
@@ -42,16 +43,15 @@ private class Fishing implements Strategy {
     public void execute() {
         //Chop the tree
 
-        if(Players.getMyPlayer().getAnimation() != 621){
-            Npcs.getNearest(315,316,317);
-
+        if(Players.getMyPlayer().getAnimation() != 622 &&!Inventory.isFull()){
+            Npcs.getNearest(328,329);
            // SceneObjects.getNearest(315,316);
             Players.getMyPlayer().interact(Menu.ACTION_CLICK_BUTTON);
-            Time.sleep(15000);
+            Time.sleep(60000);
 
         }else{
             if(Players.getMyPlayer().getAnimation() == 621)
-                Time.sleep(5000);
+                Time.sleep(75000);
         }
 
         //tree.interact(Menu.);
@@ -60,29 +60,35 @@ private class Fishing implements Strategy {
     }
 }
     private boolean FISHS_ID() {
-        if(Players.getMyPlayer().getAnimation()!= 621 && !Players.getMyPlayer().isInCombat()){
-            Npcs.getClosest(FISHS_ID);
-            Players.getMyPlayer().interact(317);
-            Time.sleep(15000);
-        }
+        for (Npc FISHS_ID : Npcs.getNearest(328, 329)) {
+            if (Players.getMyPlayer().getAnimation() != 622 && !Players.getMyPlayer().isInCombat() && !Inventory.isFull()) {
+                FISHS_ID.interact(0);
+                Time.sleep(60000);
+            }
 
-         if(FISHS_ID == null) {
-            ;
         }
+        return true;
+    }
+
             //Check if the Object is around.
           //  if (FISHS_ID != null) {
                 //Return the Tree Object.
             //    return FISHS_ID();
             //}
-        return FISHS_ID();
-    }
+
+
+
 private static class Drop implements Strategy {
 
 
     @Override
     public boolean activate() {
 
+        if(Inventory.isFull() &Inventory.containts(FISH_ID));{
+
+        }
         return true;
+
     }
 
     @Override
@@ -91,14 +97,36 @@ private static class Drop implements Strategy {
         for (Item fish : Inventory.getItems(FISH_ID)) {
             //Check if Log Exists
             if (fish != null) {
-                //Drop the Log.
+                Inventory.getItems(FISH_ID);//Drop the Log.
+
                 fish.drop();
+
                 //Using a Static Sleep here for Tutorial sake, You can use a Dynamic one.
                 Time.sleep(1000);
             }
         }
     }
 }
+    private static class Banking implements Strategy{
+
+        @Override
+        public boolean activate() {
+           if(Inventory.isFull()&&Players.getMyPlayer().getAnimation()!=621);
+            return false;
+        }
+
+        @Override
+        public void execute() {
+            Bank.getBanker().interact(0);
+                if(Bank.open()){
+                    Bank.depositAllExcept(304);
+                    Bank.close();
+                }
+
+            }
+        }
+
 
 }
+
 
