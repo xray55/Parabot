8c024c02c975f905ead2af5018bae246b0e5155d
Zachary Martin <Zach.martin.paul@live.com>
2021-10-19 19:24:26 -0500

    Add files via upload
    working somewhat

--- Sharkfisher.java
+++ Sharkfisher.java
@@ -7,10 +7,7 @@ import org.parabot.environment.scripts.Script;
 import org.parabot.environment.scripts.ScriptManifest;
 import org.parabot.environment.scripts.framework.Strategy;
 import org.rev317.min.api.methods.*;
-import org.rev317.min.api.wrappers.Item;
-import org.rev317.min.api.wrappers.Npc;
-import org.rev317.min.api.wrappers.SceneObject;
-import org.rev317.min.api.wrappers.Tile;
+import org.rev317.min.api.wrappers.*;
 
 import java.util.ArrayList;
 
@@ -21,7 +18,10 @@ public class Sharkfisher extends Script {
     public ArrayList<Strategy> strategies = new ArrayList<Strategy>();
     private static final int[] Shark_ID = {384};
     private static final int[] SharkSpot = {313, 314};
-
+   public  final  Tile[] BankSpot ={new Tile(2596,3420) ,new Tile(2586, 3418)};
+   TilePath path2 = new TilePath((BankSpot));
+   public final Tile[] Sharkspot1 = { new Tile(2596,3420) , new Tile(2602, 3420)};
+    TilePath path = new TilePath(Sharkspot1);
     @Override
     public boolean onExecute() {
         strategies.add(new Fishing());
@@ -38,23 +38,27 @@ public class Sharkfisher extends Script {
 
         // local variable to store the tree.
         boolean FISHS_ID;
-   
+
     SceneObject bank;
         @Override
         public boolean activate() {
             FISHS_ID = FISHS_ID(); // set the local Variable
-        bank = bank();
+
             //Check if we need to chop the tree
             return true;
         }
 
+
         private SceneObject bank() {
-            for (SceneObject Bank_booth : SceneObjects.getNearest(2213)) {
-                Bank_booth.interact(502);
-                if (Players.getMyPlayer().getAnimation() != 618 && !Players.getMyPlayer().isInCombat() && Inventory.isFull()) {
-                    Walking.getNearestTileTo(new Tile(2586, 3418));
-                    Bank_booth.interact(502);
-                    Bank.depositAllExcept(312);
+            if (Players.getMyPlayer().getAnimation() != 618 && !Players.getMyPlayer().isInCombat() && Inventory.isFull()) {
+                path2.traverse();
+                path2.getNextTile();
+                path2.hasReached();
+                Time.sleep(2500);
+                for (SceneObject Bank_booth : SceneObjects.getNearest(2213)) {
+                        Bank_booth.interact(1);
+                        Bank.depositAllExcept(312);
+                    Bank.close();
                     Time.sleep(1500);
                     if (Inventory.containts(312) && !Inventory.containts(384)) {
                         Walking.getNearestTileTo(new Tile(2602, 3420));
@@ -69,21 +73,26 @@ public class Sharkfisher extends Script {
 
         @Override
         public void execute() {
-            System.out.println("Scripting starting");
-            if(Players.getMyPlayer().getAnimation() != 622 &&!Inventory.isFull()){
-                Walking.getNearestTileTo(new Tile(2602, 3420));
+            if(Players.getMyPlayer().getAnimation() != 618 &&!Inventory.isFull()){
+                System.out.println("Scripting starting");
+                Time.sleep(2500);
+                path.traverse();
+               path.getNextTile();
+               path.hasReached();
+                Time.sleep(2500);
             System.out.println("On the money");
                 Npcs.getNearest(314);
-
-                // SceneObjects.getNearest(315,316);
-
                 Time.sleep(60000);
 
             }else{
                 if(Players.getMyPlayer().getAnimation() == 618)
                     Time.sleep(75000);
             }
+
             // Players.getMyPlayer().interact(Menu.ACTION_CLICK_BUTTON);
+            if(Players.getMyPlayer().getAnimation()!=618 &&Inventory.isFull()){
+                bank = bank();
+            }
 
 
         }
@@ -92,9 +101,11 @@ public class Sharkfisher extends Script {
 
 
         private boolean FISHS_ID() {
-            for (Npc FISHS_ID : Npcs.getNearest(SharkSpot)) {
-                if (Players.getMyPlayer().getAnimation() != 618 && !Players.getMyPlayer().isInCombat() && !Inventory.isFull()) {
-                    Walking.getNearestTileTo(new Tile(2602, 3420));
+            if (Players.getMyPlayer().getAnimation() != 618 && !Players.getMyPlayer().isInCombat() && !Inventory.isFull()) {
+                for (Npc FISHS_ID : Npcs.getNearest(SharkSpot)) {
+                    path.traverse();
+                    path.getNextTile();
+                    path.hasReached();
                     FISHS_ID.interact(2);
                     Time.sleep(60000);
                 }
